7839453e3d3b6d9ee0be76165da3faa82753c8bd
bbang2001 <fktmdfl2001@naver.com>
2024-04-16 19:04:34 +0900

    파일분리함

---
+++ NC1/MenuCell.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCell.swift
+//  NC1
+//
+//  Created by 리 on 4/16/24.
+//
+
+import SwiftUI
+
+struct MenuCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuCell()
+}
