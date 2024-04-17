00222c9996685a6b1dd1bbe14a203005c59c17af
bbang2001 <fktmdfl2001@naver.com>
2024-04-17 11:21:45 +0900

    리드창 꾸미기 완료!!!!

---
+++ NC1/AddMenu.swift
@@ -0,0 +1,18 @@
+//
+//  AddMenu.swift
+//  NC1
+//
+//  Created by 리 on 4/17/24.
+//
+
+import SwiftUI
+
+struct AddMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMenu()
+}
