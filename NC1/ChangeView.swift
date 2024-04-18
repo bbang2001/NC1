74d185a9a1117db837983e19fb1c32a09d76916a
bbang2001 <fktmdfl2001@naver.com>
2024-04-18 20:38:05 +0900

    멘트 바꾸기 성공 !!! contentView -> mainview로 대거이동

---
+++ NC1/ChangeView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeView.swift
+//  NC1
+//
+//  Created by 리 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeView()
+}
