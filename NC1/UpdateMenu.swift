eeba95929f644e765230c9305b577254ff30d818
bbang2001 <fktmdfl2001@naver.com>
2024-04-18 12:13:57 +0900

    필요없는 거 정리함.

---
+++ NC1/UpdateMenu.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateMenu.swift
+//  NC1
+//
+//  Created by 리 on 4/18/24.
+//
+
+import SwiftUI
+
+struct UpdateMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateMenu()
+}
